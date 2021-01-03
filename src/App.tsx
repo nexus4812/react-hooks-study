7b8640a99aa84e0baf3eac95721fab023d766205
nexus4812 <powerbatteryboy@gmail.com>
2021-01-03 14:30:15 +0900

    add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+
+type State = {
+    price: number,
+    name: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialState: State = {
+        price: 0,
+        name: ''
+    };
+
+    const [state, setState] = useState(initialState);
+
+    const increment = (): void => setState({...state, price: state.price + 1});
+    const decrement = (): void => setState({...state, price: state.price - 1});
+    const changeName = (event : React.BaseSyntheticEvent): void => setState({...state, name: event.target.value});
+
+    return (
+        <div className="App">
+            <div>
+                <p>Counter {state.price}</p>
+                <p>{state.name}</p>
+                <input type="text" onChange={changeName}/>
+                <button onClick={increment}> + 1</button>
+                <button onClick={decrement}> - 1</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
